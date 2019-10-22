d7eeaf3e9b93368358bd5c8a18bf85daeb72ec70
chris <18729901079@163.com>
2019-10-22 18:49:53 +0800

    test cherry pick

---
+++ src/main/java/com/example/demo/java/test1.java
@@ -0,0 +1,23 @@
+package com.example.demo.java;
+
+public class test1 {
+
+
+    public static void main(String[] args){
+
+        System.out.println("test cherry pick");
+
+
+
+    }
+
+
+    private static void test1(){
+
+        System.out.println("test cherry pick1111");
+
+        System.out.println("test cherry pick222");
+
+    }
+
+}
